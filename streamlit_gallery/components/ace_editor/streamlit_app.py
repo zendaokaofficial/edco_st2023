65ba1a607d6b7fe660ca5288ef1fac1a37c3c1b7
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-25 11:56:43 +0800

    Update streamlit_app.py

--- streamlit_gallery/components/ace_editor/streamlit_app.py
+++ streamlit_gallery/components/ace_editor/streamlit_app.py
@@ -1,11 +1,37 @@
+import altair as alt
 import streamlit as st
-
-from pathlib import Path
-
+import pandas as pd
+from datetime import datetime, timedelta
+import math
 
 def main():
-    st.subheader("404 Page not Found")
-
+    sheet_id = "1QsP2rfSIC5TkpqNpsccigQHO5ydps2msozFKmsoNCXk"
+    
+    source = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=1607729660")    
+    source["ID SLS"] = source["ID SLS"].astype(str)
+    source["Kode Provinsi"] = source["Kode Provinsi"].astype(str).str.zfill(2)
+    source["Kode Kab/Kota"] = source["Kode Kab/Kota"].astype(str).str.zfill(2)
+    source["Kode Kecamatan"] = source["Kode Kecamatan"].astype(str).str.zfill(3)
+    source["Kode Desa"] = source["Kode Desa"].astype(str).str.zfill(3)
+    source["Kode SLS"] = source["Kode SLS"].astype(str).str.zfill(6)
+    
+    source3 = source.groupby(["Nama Koseka", "Nama PML"])['Rating'].mean()
+    source3 = source3.reset_index()
+    source3 = source3.sort_values("Rating")
+    source3["Rating"] = source3["Rating"].apply(lambda x : round(x,2))
+    
+    x = alt.Chart(source3).encode(
+        x='Rating',
+        y='Nama PML',
+        text='Rating',
+        color=alt.condition(
+            alt.datum.Rating < 6,
+            alt.value("orange"),  # The positive color
+            alt.value("green")  # The negative color
+        )
+    )
+    
+    x.mark_bar() + x.mark_text(align='left', dx=6)
 
 if __name__ == "__main__":
     main()
