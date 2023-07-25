cc2b778004b14f07bcf6611cf8ff8296ae106ae4
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-26 06:45:13 +0800

    Update streamlit_app.py

--- streamlit_gallery/components/disqus/streamlit_app.py
+++ streamlit_gallery/components/disqus/streamlit_app.py
@@ -1,13 +1,40 @@
+import altair as alt
 import streamlit as st
-
-from streamlit_disqus import st_disqus
-from streamlit_gallery.utils.readme import readme
-
+import pandas as pd
+from datetime import datetime, timedelta
+import math
+import numpy as np 
 
 def main():
-    with readme("streamlit-disqus", st_disqus, __file__):
-        st_disqus("streamlit-disqus-demo")
+    sheet_id = "1QsP2rfSIC5TkpqNpsccigQHO5ydps2msozFKmsoNCXk"
+    
+    source = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=1607729660")    
+    source["ID SLS"] = source["ID SLS"].astype(str)
+    source["Kode Provinsi"] = source["Kode Provinsi"].astype(str).str.zfill(2)
+    source["Kode Kab/Kota"] = source["Kode Kab/Kota"].astype(str).str.zfill(2)
+    source["Kode Kecamatan"] = source["Kode Kecamatan"].astype(str).str.zfill(3)
+    source["Kode Desa"] = source["Kode Desa"].astype(str).str.zfill(3)
+    source["Kode SLS"] = source["Kode SLS"].astype(str).str.zfill(6)
+    
+    source3 = source.groupby(["Nama Koseka", "Nama PML"])['Rating'].mean()
+    source3 = source3.reset_index()
+    source3 = source3.sort_values("Rating")
+    source3["Rating"] = source3["Rating"].apply(lambda x : round(x,2))
 
+    source2 = source3[source3["Jumlah L2"].notnull()]
+
+    source2 = source2[source2["Status"] == "Dikembalikan ke Koseka"]
+    source2.loc[:,"Waktu"] = pd.to_datetime(source2["Waktu"], format = "%m/%d/%Y")
+
+    source2["Waktu Saat ini"] = np.datetime64('today')
+
+    source2["Lama Pengembalian"] = source2["Waktu Saat ini"] - source2["Waktu"]
+
+    source4 = source2.loc[:,["Nama Kecamatan", "Nama Desa", "Nama SLS", "Nama Koseka", "Nama PML", "Waktu", "Lama Pengembalian"]].reset_index(drop=True)
+
+    st.subheader("Dokumen Dikembalikan")
+
+    st.dataframe(source4.reset_index(drop = True), use_container_width=True)
 
 if __name__ == "__main__":
     main()
