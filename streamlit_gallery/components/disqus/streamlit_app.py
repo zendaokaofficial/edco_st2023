a12de5408cf884f8b14219a9da206c5d7b85e930
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-26 06:53:56 +0800

    Update streamlit_app.py

--- streamlit_gallery/components/disqus/streamlit_app.py
+++ streamlit_gallery/components/disqus/streamlit_app.py
@@ -28,7 +28,7 @@ def main():
 
     source4 = source2.loc[:,["Nama Kecamatan", "Nama Desa", "Nama SLS", "Nama Koseka", "Nama PML", "Waktu", "Lama Pengembalian"]].reset_index(drop=True)
 
-    source4["Lama Pengembalian"] = source4["Lama Pengembalian"].astype(str)
+    source4["Lama Pengembalian"] = source4["Lama Pengembalian"].astype('timedelta64[D]').astpe(str)
 
     st.subheader("Dokumen Dikembalikan")
 
